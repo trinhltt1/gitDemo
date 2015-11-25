57f515274a2e299c4377c2bc5602b3df6bbc3936
Trinh Le <trinhle.bhtech@gmail.com>
2015-11-25 10:06:28 +0700

    initial project commit

---
+++ gitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitDemo
+//
+//  Created by TrinhLe on 11/25/15.
+//  Copyright (c) 2015 TrinhLe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitDemo
+//
+//  Created by TrinhLe on 11/25/15.
+//  Copyright (c) 2015 TrinhLe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
