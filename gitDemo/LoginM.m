29187ee892e1c776f136bbf8d5f31d09d58f7f51
Trinh Le <trinhle.bhtech@gmail.com>
2015-11-25 10:13:32 +0700

    imp Login MVC template

---
+++ gitDemo/LoginM.m
@@ -0,0 +1,20 @@
+//
+//  LoginM.m
+//  gitDemo
+//
+//  Created by TrinhLe on 11/25/15.
+//  Copyright (c) 2015 TrinhLe. All rights reserved.
+//
+// Generated with Xcode template from https://github.com/haydump/MVCxcodeTemplate
+//
+
+#import "LoginM.h"
+
+@interface LoginM ()
+
+@end
+
+
+@implementation LoginM
+
+@end
