5c72462b649afba5903e2866fbf746602ce9aef2
Trinh Le <trinhle.bhtech@gmail.com>
2015-11-27 16:59:27 +0700

    imp testClass

---
+++ testClass.h
@@ -0,0 +1,13 @@
+//
+//  testClass.h
+//  gitDemo
+//
+//  Created by TrinhLe on 11/27/15.
+//  Copyright (c) 2015 TrinhLe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testClass : UIView
+
+@end
